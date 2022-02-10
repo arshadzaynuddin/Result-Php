c941b75fd4b59ab52d2ea2c748545dbd2093795d
arshadzaynuddin <arshadzaynuddin@gmail.com>
2022-02-10 18:55:16 +0530

    Completed

--- insert.php
+++ insert.php
@@ -4,6 +4,10 @@ $a=$_POST['rollno'];
 $b=$_POST['name'];
 $c=$_POST['mark'];
 $d=$_POST['grade'];
-
-
+$r=pg_query($con,"INSERT INTO public.student (rollno,name,mark,grade)VALUES('$a','$b','$c','$d')");
+ if($r){
+     echo "success";
+ }else{
+     echo "error";
+ }
 ?>
