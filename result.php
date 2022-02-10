b2e9d684d38937e0f29f65c6765b3aa2937a337c
arshadzaynuddin <arshadzaynuddin@gmail.com>
2022-02-10 19:28:00 +0530

    Design Modified

--- result.php
+++ result.php
@@ -2,9 +2,17 @@
 <html>
 <head>
 <title>Result</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    
 <link rel="stylesheet" href="/style.css">
 </head>
+<body>
+    
+<h3 align="center">Result</h3>
+
     <div class="container">
+<table class="table table-striped table-dark" align="center">
 <?php
 include ("config.php");
 $a=$_POST['regno'];
@@ -14,12 +22,24 @@ $f=0;
 while($row=pg_fetch_row($r))
 {
     if($a==$row[0])
-    {
-     
-    echo "Register Number:" . $row[0] . "<br>";
-    echo "<b>Name:</b>" ."<b> $row[1]</b>" . "<br></b>";
-    echo "Mark:" . $row[2] . "<br>";
-    echo "Grade:" . $row[3] . "<br>";
+    {   
+        echo "<tr><td>Name</td><td>$row[1]</td></tr>";
+        echo "<tr>";
+        echo "<td>Register No</td>";
+        echo "<td> $row[0]</td>";
+        echo "</tr>";
+
+       
+        echo "<tr>";
+        echo "<td>Mark</td>";
+        echo "<td> $row[2]</td>";
+        echo "</tr>";
+
+        echo "<tr>";
+        echo "<td>Grade</td>";
+        echo "<td> $row[3]</td>";
+        echo "</tr>";
+        echo "</table>";
     $f=1;
     }
 }
@@ -27,5 +47,7 @@ if($f==0)
 echo "Result not found";
 pg_close($con);
 ?>
-    </div>
+</table>
+</body>
+</div>
 </HTML>
